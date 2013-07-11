02955666a9a6747a3a23bec53d6241ad67832584
Balaji Iyengar <bbalaji8@gmail.com>
2013-07-11 17:20:39 +0000

    Modified grader.js to accept URL on command line as an option

--- grader.js
+++ grader.js
@@ -24,15 +24,40 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://.html";
+var TEMPHTMLFILE = "temp.html";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	console.log("%s does not exist. Exiting.", instr);
+	process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
+    return instr;
+};
+
+var assertURLExists = function(inurl) {
+    var instr = inurl.toString();
+    // console.log("assertURLExists --> instr = %s", instr);
+
+    rest.get(instr).on('complete', function(result) {
+	if (result instanceof Error) {
+	    console.log("Error with %s: %s. Exiting.", instr, result.message);
+	    process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	} else {
+	    //console.log("assertURLExists --> result = %s", result.toString());
+	    fs.writeFile(TEMPHTMLFILE, result, function (err) {
+		if (err instanceof Error) {
+		    console.log("Error with writing TEMPFILE: %s. Exiting.", err.message);
+		}
+		// console.log("TEMPHTMLFILE saved !");
+});
+	}
+    });
+
     return instr;
 };
 
@@ -49,8 +74,8 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
+	var present = $(checks[ii]).length > 0;
+	out[checks[ii]] = present;
     }
     return out;
 };
@@ -63,10 +88,24 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+	.option('-u, --url <url_path>', 'Path to url',clone(assertURLExists), URL_DEFAULT)
+	.parse(process.argv);
+
+    // console.log("program.file = %s", program.file);
+    // console.log("program.checks = %s", program.checks);
+    // console.log("program.url = %s", program.url);
+
+    var fileName = program.file;
+    if (program.url != URL_DEFAULT) {
+	// console.log("program.url is not default");
+	fileName = TEMPHTMLFILE;
+    } else {
+	// console.log("program.url is default");
+    }
+
+    var checkJson = checkHtmlFile(fileName, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
